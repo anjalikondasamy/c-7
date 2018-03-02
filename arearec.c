212536f92e7580beabc4f53c0de9dea263ddb94b
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-02 02:29:36 -0800

    arearec.c
    good

---
+++ arearec.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int length,breadth,area;
+clrscr();
+
+printf("\n Enter the lenth");
+scanf("%d",&length);
+
+printf("\n Enter the breadth");
+scanf("%d",&breadth);
+
+area=length*breadth;
+printf("area")
+return0;
+}
